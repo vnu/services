53ab5ea58ddce21629b96c551e95df475e51dcc1
Vnu <dearvnu@yahoo.in>
2012-08-28 19:07:57 -0400

    new location logger update started

--- src/edu/buffalo/cse/phonelab/services/statusmonitor/StatusMonitorParameters.java
+++ src/edu/buffalo/cse/phonelab/services/statusmonitor/StatusMonitorParameters.java
@@ -1,5 +1,16 @@
 package edu.buffalo.cse.phonelab.services.statusmonitor;
 
+import org.simpleframework.xml.Element;
+
 public class StatusMonitorParameters {
+	
+	@Element
+	public String logTag;
+	
+	@Element
+	public Integer checkInterval;
+	
+	@Element(type=ServiceIntervals.class)
+	public ServiceIntervals serviceInterval;
 
 }
--- src/edu/buffalo/phonelab/cse/services/statuslogger/LocationLogger.java
+++ src/edu/buffalo/phonelab/cse/services/statuslogger/LocationLogger.java
@@ -1,5 +1,27 @@
 package edu.buffalo.phonelab.cse.services.statuslogger;
 
+import android.location.Location;
+import android.util.Log;
+
 public class LocationLogger {
+	
+	public static String mLatitude;
+	public static String mLongitude;
+	public static String mAccuracy;
+	public static String mProvider;
+	public static String mSpeed;
+	public static String mLocationFixTime;
+	
+	public static void updateLocation(Location location) {
+		Log.i("Status-passive","Update Successful");
+		mLatitude = String.valueOf(location.getLatitude());
+		mLongitude = String.valueOf(location.getLongitude());
+		mAccuracy = String.valueOf(location.getAccuracy());
+		mProvider = location.getProvider();
+		mSpeed = String.valueOf(location.getSpeed());
+		mLocationFixTime = String.valueOf(location.getTime());
+		Log.i("Status-active","Update Successful with Latitude : "+mLatitude+" Prov : "+mProvider);
+
+	}
 
 }
